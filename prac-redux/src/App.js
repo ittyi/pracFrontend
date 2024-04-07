f63453a5223f531f2db090c12bb9ec62d4190581
ittyi <vioiv.k4@gmail.com>
2024-04-07 17:42:10 +0900

    なるほど。reducers の　increment, decrement 、useSelector, useDispatchの使い方なんとなく理解

--- prac-redux/src/App.js
+++ prac-redux/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Counter} from './features/counter/Counter';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <Counter/>
+      </main>
     </div>
   );
 }
